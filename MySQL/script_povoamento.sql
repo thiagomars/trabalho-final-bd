86787d4ebaa975529b6708d903ea2200213d388e
Thiago Marques Sousa <50245545+thiagomars@users.noreply.github.com>
2022-06-27 20:04:21 -0300

    Update script_povoamento.sql

--- MySQL/script_povoamento.sql
+++ MySQL/script_povoamento.sql
@@ -1,1 +1,23 @@
-h
+/* POPULANDO A TABELA CATEGORIA */
+INSERT INTO categoria VALUES ("aventura", 202212341, "aventura é uma categoria de livros etc");
+INSERT INTO categoria VALUES ("fantasia", 202212343, "fantasia é uma categoria de livros etc");
+INSERT INTO categoria VALUES ("tecnologia e ciencias", 202212344, "tecnologia e ciencias é uma categoria de livros etc");
+INSERT INTO categoria VALUES ("romance", 202212345, "romance é uma categoria de livros etc");
+INSERT INTO categoria VALUES ("psicologia_desenvolvimento_saude", 202212346, "Psicologia do Desenvolvimento Saúde, Boa Forma e Dieta é uma categoria de livros etc");
+INSERT INTO categoria VALUES ("editorial", 202212347, "editorial é uma categoria de livros etc");
+INSERT INTO categoria VALUES ("auto_biografia", 202212348, "auto biografia é uma categoria de livros etc");
+
+
+/* POPULANDO A TABELA LIVROS */
+INSERT INTO livros VALUES (202212341, "editorial", 20051120, 3, "Aventurando nas Aventuras do Aventureiro", "aventura");
+INSERT INTO livros VALUES (202212342, "fundação qualquer", 19981120, 5, "O EU descrito", "auto_biografia");
+INSERT INTO livros VALUES (202212343, "editorial", 20180701, 2, "O nome do vento", "fantasia");
+INSERT INTO livros VALUES (202212344, "ditoriando", 19990213, 18, "Ciencia Hoje, Tecnologia Amanhã", "tecnologia e ciencias");
+INSERT INTO livros VALUES (202212345, "ditoriando", "20161201", 17, "Uma Breve História do Tempo", "tecnologia e ciencias");
+INSERT INTO livros VALUES (202212346, "outrali", 19870820, 1, "A culpa é das estrelas", "romance");
+INSERT INTO livros VALUES (202212347, "Padrão", "20200820", 2, "Mindset: A nova psicologia do sucesso", "psicologia_desenvolvimento_saude");
+INSERT INTO livros VALUES (202212348, "outrali", 20180820, 1, "O Homem de Giz", "fantasia");
+INSERT INTO livros VALUES (202212349, "outrali", 20190820, 1, "A arte da guerra", "fantasia");
+INSERT INTO livros VALUES (202212350, "ditoriando", 20140820, 33, "Ensinando algo", "tecnologia e ciencias");
+INSERT INTO livros VALUES (202212351, "outrali", 19990820, 1, "Como caçar um gato", "editorial");
+INSERT INTO livros VALUES (202212352, "fundação qualquer", 20210820, 5, "Biografia do EU", "auto_biografia");
