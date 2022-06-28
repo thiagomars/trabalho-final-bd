d557674f54d8a16e6cbd213b8638e8fd18ae5ec3
Thiago Marques Sousa <50245545+thiagomars@users.noreply.github.com>
2022-06-28 20:30:21 -0300

    Create script_trigger.sql

---
+++ MySQL/script_trigger.sql
@@ -0,0 +1,16 @@
+/*Trigger que verifica se a data prevista de formção do usuário em questão ainda pode realizar empréstimo*/
+DELIMITER //
+CREATE TRIGGER verifica_emprestimo
+BEFORE INSERT ON registros
+FOR EACH ROW
+BEGIN
+	IF NEW.data > (
+		SELECT al.data_formacao_prevista 
+		FROM alunos al, usuarios us
+		WHERE us.usuario = al.usuario
+    ) + 15 THEN
+		SET NEW.data = 10101010;
+    END IF;
+END;
+//
+DELIMITER ;
